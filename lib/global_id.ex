77606d987fb682ba4a8ba5b866b30945da40abb7
Andrew Forward <aforward@gmail.com>
2020-05-22 13:15:51 -0400

    Implement node_id as an input parameter to the GenServer

--- lib/global_id.ex
+++ lib/global_id.ex
@@ -17,26 +17,15 @@ defmodule GlobalId do
   same microsecond, but this is a good start.
   """
   @spec get_id(pid()) :: non_neg_integer
-  def get_id(pid) do
-    <<n::size(64)>> = <<node_id()::10, timestamp()::47, next_id(pid)::7>>
-    n
-  end
-
-  @doc """
-  Return a locally unique non-negative integer.
-  Provide the process of the GlobalId GenServer you
-  are connecting you.
-  """
-  @spec next_id(pid()) :: non_neg_integer
-  def next_id(pid), do: GenServer.call(pid, :next_id)
+  def get_id(pid), do: GenServer.call(pid, :get_id)
 
   @doc """
   Returns your node id as an integer.
   It will be greater than or equal to 0 and less than 1024.
   It is guaranteed to be globally unique.
   """
-  @spec node_id() :: non_neg_integer
-  def node_id, do: 18
+  @spec node_id(pid()) :: non_neg_integer
+  def node_id(pid), do: GenServer.call(pid, :node_id)
 
   @doc """
   Returns timestamp since the epoch in microsecond.
@@ -53,20 +42,26 @@ defmodule GlobalId do
   Start our GlobalId GenServer, with our next_id being 0.
 
   You can start your server with `start_link/2` and then
-  send the message `:next_id` the PID as shown below.
+  send the message `:get_id` the PID as shown below.
 
-      iex> {:ok, pid} = GenServer.start_link(GlobalId, :ok)
-      iex> GenServer.call(pid, :next_id)
+      iex> {:ok, pid} = GenServer.start_link(GlobalId, 18)
+      iex> GenServer.call(pid, :get_id)
 
   These behaviours are also captured in the API above.
   """
   @impl true
-  def init(_) do
-    {:ok, %{counter: 0}}
+  def init(node_id) do
+    {:ok, %{node_id: node_id, counter: 0}}
   end
 
   @impl true
-  def handle_call(:next_id, _from, %{counter: counter}) do
-    {:reply, counter, %{counter: counter + 1}}
+  def handle_call(:node_id, _from, %{node_id: node_id} = state) do
+    {:reply, node_id, state}
+  end
+
+  @impl true
+  def handle_call(:get_id, _from, %{node_id: node_id, counter: counter} = state) do
+    <<id::size(64)>> = <<node_id::10, timestamp()::47, counter::7>>
+    {:reply, id, %{state | counter: counter + 1}}
   end
 end
